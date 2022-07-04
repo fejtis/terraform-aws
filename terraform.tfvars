be64e2a14407588042205bbf75c1e67a9d0ad3e1
fejtis <ondrejfejtek@gmail.com>
2022-07-04 07:26:59 +0000

    initial commit

---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+vpc_cidr               = "10.20.0.0/16"
+private_subnet_01_cidr = "10.20.1.0/24"
+public_subnet_01_cidr  = "10.20.2.0/24"
---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {
+  type        = string
+  default     = "eu-west-1"
+  description = "AWS region"
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "private_subnet_01_cidr" {
+  type = string
+}
+
+variable "public_subnet_01_cidr" {
+  type = string
+}
